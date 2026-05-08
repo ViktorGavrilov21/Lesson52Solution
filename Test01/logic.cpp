a8c85a23aae6135f82ece5f9b7824d334fec8f86
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-05-08 18:00:43 +0300

    update logic

--- Test01/logic.cpp
+++ Test01/logic.cpp
@@ -5,13 +5,18 @@ void sort_bubble(int array[], int size) {
 		return;
 	}
 
-	for (int i = 0; i < (size - 1); i++) {
-		for (int j = 0; j < (size - 1); j++) {
+	for (int i = 0; i < size - 1; i++) {
+		bool flag = true;
+		for (int j = 0; j < (size - 1 - i); j++) {
 			if (array[j] > array[j + 1]) {
+				flag = false;
 				int t = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = t;
 			}
+		}
+		if (flag) {
+			break;
 		}
 	}
 }
--- Test01/main.cpp
+++ Test01/main.cpp
@@ -1,6 +1,8 @@
 #include "logic.h"
 
 int main() {
+	//int array[]{ 9, 1, 2, 3, 4, 5, 6, 7, 8 };
+	//int size = 9;
 	int array[DEFAULT_SIZE];
 	int size;
 
